c3221a63cc1c0787c68412c2f3b8ceedec247686
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-03 01:05:42 +0430

    import AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { UserModule } from './user/user.module'
 import { PostModule } from './post/post.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [UserModule, PostModule, PrismaModule]
+	imports: [UserModule, PostModule, PrismaModule, AuthModule]
 })
 export class AppModule {}
