c8a0cf5ecd153925daca655535844cc4cf5b43fe
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-03 06:41:09 +0430

    fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { UserRepository } from './users.repository'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [UserController],
-	providers: [UserService]
+	providers: [UserService, PrismaService, UserRepository]
 })
 export class UserModule {}
