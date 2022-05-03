598ac11840a4e58b91b502bdbe4baf765084a6ce
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-03 06:41:00 +0430

    change mailer config

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -3,22 +3,12 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter'
 import { Module } from '@nestjs/common'
 import { MailService } from './mail.service'
 import { join } from 'path'
+import { mailerConfig } from 'src/config'
 
 @Module({
 	imports: [
 		MailerModule.forRoot({
-			transport: {
-				host: 'smtp.mailtrap.io',
-				port: 2525,
-				secure: false,
-				auth: {
-					user: '043b6ba33c1069',
-					pass: 'fabf1b61c51212'
-				}
-			},
-			defaults: {
-				from: '"No Reply" <noreply@example.com>'
-			},
+			...mailerConfig,
 			template: {
 				dir: join(__dirname, 'templates'),
 				adapter: new EjsAdapter(),
