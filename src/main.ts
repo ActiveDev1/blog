d347ef3fc30d190352bca7127ac3e298532cf252
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-04 00:58:39 +0430

    colorize fastify log

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,20 @@ import {
 	FastifyAdapter,
 	NestFastifyApplication
 } from '@nestjs/platform-fastify'
-import { Logger, ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import { config } from './config'
 
 async function bootstrap() {
-	const logger = new Logger('Bootstrap')
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
-		new FastifyAdapter({ logger: config.server.restApi.logger })
+		new FastifyAdapter({
+			logger: config.server.restApi.logger
+				? { prettyPrint: { colorize: true } }
+				: false
+		})
 	)
 	app.useGlobalPipes(new ValidationPipe())
 	await app.listen(config.server.restApi.port, config.server.restApi.host)
-	logger.log(`Application is running on: ${await app.getUrl()}`)
 }
 
 bootstrap()
