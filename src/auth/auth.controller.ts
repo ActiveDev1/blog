791f5870731306dcbcb8a529c6b490f542fa8cb3
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-04 00:01:46 +0430

    add signup, refreshToken controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { User } from '@prisma/client'
 import { AuthService } from './auth.service'
+import { GetUser } from './decorators/get-user.decorator'
 import { GetEmailDto } from './dtos/get-email.dto'
+import { GetSignupVerificationDto } from './dtos/get-signup-verification-dto.dto'
+import { ITokens } from './interfaces/tokens.interface'
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +14,18 @@ export class AuthController {
 	@Post('signup/code')
 	async sendSignupCode(@Body() getEmailDto: GetEmailDto): Promise<void> {
 		return await this.authService.sendSignupCode(getEmailDto)
+	}
+
+	@Post('signup')
+	async signup(
+		@Body() getSignupVerificationDto: GetSignupVerificationDto
+	): Promise<ITokens> {
+		return await this.authService.signup(getSignupVerificationDto)
+	}
+
+	@Post('refresh')
+	@UseGuards(AuthGuard('refresh'))
+	async refreshToken(@GetUser() user: User): Promise<ITokens> {
+		return await this.authService.verifyRefreshToken(user.id)
 	}
 }
