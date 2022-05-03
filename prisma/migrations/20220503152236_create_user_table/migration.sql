8dda0ce9c40e958143d7f5210a2942ad8bddd810
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-03 19:54:00 +0430

    set password, deletedAt default null

--- prisma/migrations/20220503152236_create_user_table/migration.sql
+++ prisma/migrations/20220503152236_create_user_table/migration.sql
@@ -4,10 +4,10 @@ CREATE TABLE `User` (
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `deletedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
