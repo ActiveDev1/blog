55bd0dc007e8d5560b79a0639780ff45f188c790
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-04 21:30:29 +0430

    add api doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,28 +4,58 @@ import { User } from '@prisma/client'
 import { AuthService } from './auth.service'
 import { GetUser } from './decorators/get-user.decorator'
 import { GetEmailDto } from './dtos/get-email.dto'
-import { GetSignupVerificationDto } from './dtos/get-signup-verification-dto.dto'
-import { ITokens } from './interfaces/tokens.interface'
+import { GetSignupVerificationDto } from './dtos/get-signup-verification.dto'
+import {
+	ApiCreatedResponse,
+	ApiUnprocessableEntityResponse,
+	ApiBody,
+	ApiHeader,
+	ApiTags
+} from '@nestjs/swagger'
+import { Tokens } from './dtos/tokens.dto'
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
 	@Post('signup/code')
+	@ApiBody({ type: GetEmailDto })
+	@ApiCreatedResponse({
+		description: 'A verification code sent to target email'
+	})
 	async sendSignupCode(@Body() getEmailDto: GetEmailDto): Promise<void> {
 		return await this.authService.sendSignupCode(getEmailDto)
 	}
 
 	@Post('signup')
+	@ApiBody({ type: GetSignupVerificationDto })
+	@ApiCreatedResponse({
+		description: 'User created',
+		type: Tokens
+	})
+	@ApiUnprocessableEntityResponse({
+		description: 'Wrong code received'
+	})
 	async signup(
 		@Body() getSignupVerificationDto: GetSignupVerificationDto
-	): Promise<ITokens> {
+	): Promise<Tokens> {
 		return await this.authService.signup(getSignupVerificationDto)
 	}
 
 	@Post('refresh')
 	@UseGuards(AuthGuard('refresh'))
-	async refreshToken(@GetUser() user: User): Promise<ITokens> {
+	@ApiHeader({
+		name: 'Authorization',
+		allowEmptyValue: false,
+		description: 'Must be refresh token',
+		example: 'Bearer JWT-TOKEN'
+	})
+	@ApiCreatedResponse({
+		description: 'Access & Refresh tokens',
+		type: Tokens
+	})
+	async refreshToken(@GetUser() user: User): Promise<Tokens> {
 		return await this.authService.verifyRefreshToken(user.id)
 	}
 }
