0021a577637365f33412d959b877f6116ee0c44e
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-11 01:52:14 +0430

    add CreatePostDto & GetPostIdDto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePostDto {}
+import { IsBoolean, IsString, IsOptional, IsNotEmpty } from 'class-validator'
+import { Transform } from 'class-transformer'
+
+export class CreatePostDto {
+	@IsString()
+	@IsNotEmpty()
+	@Transform((value) => value.value.trim())
+	title: string
+
+	@IsString()
+	@IsNotEmpty()
+	body: string
+
+	@IsString()
+	@IsOptional()
+	description: string
+
+	@IsString({ always: false })
+	@IsOptional()
+	@Transform((value) => value.value.trim())
+	slug: string
+
+	@IsBoolean()
+	@IsOptional()
+	isPublished: boolean
+}
