2fe7c72a37a51981b54f37732577bb54491a3797
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-11 01:50:12 +0430

    add generateRandomString & slugify

--- src/common/utils/helpers/functions.ts
+++ src/common/utils/helpers/functions.ts
@@ -16,4 +16,23 @@ function generateRandomNumber(): number {
 	return _.random(100000, 999999)
 }
 
-export { generateSignupCode, generateUsernameFromEmail, generateRandomUsername }
+function generateRandomString(): string {
+	return Math.random().toString(36).slice(2)
+}
+
+function slugify(string: string): string {
+	return string
+		.replace(/^\s+|\s+$/g, '')
+		.toLocaleLowerCase()
+		.replace(/[^a-z0-9_\s-ءاأإآؤئبتثجحخدذرزسشصضطظعغفقكلمنهويةى]#u/, '')
+		.replace(/\s+/g, '-')
+		.replace(/-+/g, '-')
+}
+
+export {
+	generateSignupCode,
+	generateUsernameFromEmail,
+	generateRandomUsername,
+	generateRandomString,
+	slugify
+}
