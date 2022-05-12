a39f0803412ca9b0523b96836c0a6cdad314b0e4
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-12 20:28:31 +0430

    fix doc

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,26 +1,32 @@
 import { IsBoolean, IsString, IsOptional, IsNotEmpty } from 'class-validator'
 import { Transform } from 'class-transformer'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 
 export class CreatePostDto {
 	@IsString()
 	@IsNotEmpty()
 	@Transform((value) => value.value.trim())
+	@ApiProperty()
 	title: string
 
 	@IsString()
 	@IsNotEmpty()
+	@ApiProperty()
 	body: string
 
 	@IsString()
 	@IsOptional()
+	@ApiPropertyOptional()
 	description: string
 
 	@IsString({ always: false })
 	@IsOptional()
 	@Transform((value) => value.value.trim())
+	@ApiPropertyOptional()
 	slug: string
 
 	@IsBoolean()
 	@IsOptional()
+	@ApiPropertyOptional({ default: false })
 	isPublished: boolean
 }
