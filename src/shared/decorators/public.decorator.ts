8f59bb3e808ac0813d7c5644f1a210cc775a4934
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-13 02:14:37 +0430

    add Public decorator

--- src/shared/decorators/public.decorator.ts
+++ src/shared/decorators/public.decorator.ts
@@ -1,2 +1,6 @@
 import { SetMetadata } from '@nestjs/common'
+
+/**
+ * Decorator that ignore the guards of the scope of the controller or method.
+ */
 export const Public = () => SetMetadata('isPublic', true)
