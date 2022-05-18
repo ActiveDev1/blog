7a363784ec29a2fb39b875e35b6cfdbac20c9f7c
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-05-18 23:48:20 +0430

    import AuthModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { UserRepository } from './users.repository'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
+	imports: [AuthModule],
 	controllers: [UserController],
 	providers: [UserService, UserRepository]
 })
