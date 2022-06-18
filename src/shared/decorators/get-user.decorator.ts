f735ae0debb97b67e32b53262bcd699d3d63e00d
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-06-18 17:52:03 +0430

    refactor GetUser decorator

--- src/shared/decorators/get-user.decorator.ts
+++ src/shared/decorators/get-user.decorator.ts
@@ -1,9 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 import { User } from '@prisma/client'
+import { FastifyRequest } from 'fastify'
 
 /**
  * Decorator that get user data from req.user.
  */
 export const GetUser = createParamDecorator((_data: never, ctx: ExecutionContext): User => {
-	return ctx.switchToHttp().getRequest()['user']
+	const { user } = ctx.switchToHttp().getRequest<FastifyRequest>()
+	return user
 })
